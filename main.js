328726589d45fd58984825b302ea4fc993d3a6d0
Luis Gustavo de Freitas Antunes - 12924111583 <12924111583@ulife.com.br>
2024-08-22 09:22:20 -0300

    add difficulty levels

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const background = document.querySelector(".background");
 const button = document.querySelector(".button");
 const score = document.querySelector(".score");
+const level = document.querySelector(".level");
+const levelNumber = document.querySelector(".level-number");
+levelNumber.innerHTML = 1;
 score.innerHTML = 0;
 score.style.color = "red";
 background.style.backgroundColor = "gray";
@@ -8,6 +11,7 @@ let leftButtonPosition = 42;
 let topButtonPosition = 42;
 button.style.left = `${leftButtonPosition}vw`;
 button.style.top = `${topButtonPosition}vh`;
+let levelMultiplier = 10;
 
 document.addEventListener("click", (e) => manageClick(e));
 
@@ -32,34 +36,41 @@ function manageClick(e) {
 
 setInterval(() => {
     const rng = Math.round(Math.random() * 100);
-    if (leftButtonPosition < 15 || topButtonPosition < 20) {
+    if (leftButtonPosition < 10 || topButtonPosition < 20) {
         leftButtonPosition += Math.random() + 5;
         topButtonPosition += Math.random() + 2.5;
     }
-    if (leftButtonPosition > 85 || topButtonPosition > 80) {
+    else if (leftButtonPosition > 90 || topButtonPosition > 80) {
         leftButtonPosition -= Math.random() + 5;
         topButtonPosition -= Math.random() + 2.5;
     }
-    if (rng >= 0 && rng < 25) {
+    else if (rng >= 0 && rng < 25) {
         leftButtonPosition += Math.random();
         topButtonPosition += Math.random();
     } 
-    if (rng >= 25 && rng < 50){
+    else if (rng >= 25 && rng < 50){
         leftButtonPosition -= Math.random();
         topButtonPosition -= Math.random();
     }
-    if (rng >= 50 && rng < 75) {
+    else if (rng >= 50 && rng < 75) {
         leftButtonPosition += Math.random();
         topButtonPosition -= Math.random();
     }
-    if (rng >= 75 && rng <= 100) {
+    else if (rng >= 75 && rng <= 100) {
         leftButtonPosition -= Math.random();
         topButtonPosition += Math.random();
     }
     changeButtonLocation(leftButtonPosition, topButtonPosition)
-}, 10);
+}, levelMultiplier);
 
 function changeButtonLocation(left, top) {
     button.style.left = `${left}vw`;
     button.style.top = `${top}vh`;
 }
+
+level.addEventListener("click", () => {
+    levelMultiplier += 10;
+    levelNumber.innerHTML++;
+    console.log(levelMultiplier);
+    console.log(levelNumber.innerHTML);
+})
