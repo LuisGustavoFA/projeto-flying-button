cf1df5a0a09140295aba20d5ca84d01cf7804755
Luis Gustavo de Freitas Antunes - 12924111583 <12924111583@ulife.com.br>
2024-08-23 09:00:50 -0300

    add difficulty fix

--- main.js
+++ main.js
@@ -1,66 +1,52 @@
-const background = document.querySelector(".background");
 const button = document.querySelector(".button");
 const score = document.querySelector(".score");
 const level = document.querySelector(".level");
 const levelNumber = document.querySelector(".level-number");
 levelNumber.innerHTML = 1;
 score.innerHTML = 0;
-score.style.color = "red";
-background.style.backgroundColor = "gray";
 let leftButtonPosition = 42;
 let topButtonPosition = 42;
 button.style.left = `${leftButtonPosition}vw`;
 button.style.top = `${topButtonPosition}vh`;
-let levelMultiplier = 10;
+let levelMultiplier = 100;
 
-document.addEventListener("click", (e) => manageClick(e));
-
-function manageClick(e) {
-    let colorA = "black";
-    let colorB = "gray";
-
-    if (background.style.backgroundColor == colorA) {
-        background.style.backgroundColor = colorB;
-        score.style.color = colorA;
-    } else {
-        background.style.backgroundColor = colorA;
-        score.style.color = colorB;
-    }
-
+document.addEventListener("click", (e) => {
     if (e.target.className == button.className) score.innerHTML++;
-    else {
-        score.innerHTML = 0;
-        score.style.color = "red";
-    }
-}
+    else score.innerHTML = 0;
+});
 
 setInterval(() => {
     const rng = Math.round(Math.random() * 100);
+    const correcaoX = Math.random() + 5;
+    const correcaoY = Math.random() + 2.5;
+    const randomDirection = (Math.random() * 2) - (levelMultiplier / 100);
+
     if (leftButtonPosition < 10 || topButtonPosition < 20) {
-        leftButtonPosition += Math.random() + 5;
-        topButtonPosition += Math.random() + 2.5;
+        leftButtonPosition += correcaoX;
+        topButtonPosition += correcaoY;
     }
     else if (leftButtonPosition > 90 || topButtonPosition > 80) {
         leftButtonPosition -= Math.random() + 5;
         topButtonPosition -= Math.random() + 2.5;
     }
     else if (rng >= 0 && rng < 25) {
-        leftButtonPosition += Math.random();
-        topButtonPosition += Math.random();
+        leftButtonPosition += randomDirection;
+        topButtonPosition += randomDirection;
     } 
     else if (rng >= 25 && rng < 50){
-        leftButtonPosition -= Math.random();
-        topButtonPosition -= Math.random();
+        leftButtonPosition -= randomDirection;
+        topButtonPosition -= randomDirection;
     }
     else if (rng >= 50 && rng < 75) {
-        leftButtonPosition += Math.random();
-        topButtonPosition -= Math.random();
+        leftButtonPosition += randomDirection;
+        topButtonPosition -= randomDirection;
     }
     else if (rng >= 75 && rng <= 100) {
-        leftButtonPosition -= Math.random();
-        topButtonPosition += Math.random();
+        leftButtonPosition -= randomDirection;
+        topButtonPosition += randomDirection;
     }
     changeButtonLocation(leftButtonPosition, topButtonPosition)
+    console.log(levelMultiplier);
 }, levelMultiplier);
 
 function changeButtonLocation(left, top) {
@@ -69,8 +55,14 @@ function changeButtonLocation(left, top) {
 }
 
 level.addEventListener("click", () => {
-    levelMultiplier += 10;
-    levelNumber.innerHTML++;
+    if (levelNumber.innerHTML < 4) {
+        levelNumber.innerHTML++;
+        levelMultiplier -= 30;
+        
+    } else {
+        levelNumber.innerHTML = 1;
+        levelMultiplier = 100;
+    }
     console.log(levelMultiplier);
     console.log(levelNumber.innerHTML);
 })
